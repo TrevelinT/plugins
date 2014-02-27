d990cc08020ede4775ce77306db1521b6ecf34aa
Danilo Muller <danilo.muller.trevelin@gmail.com>
2014-02-27 16:12:03 -0300

    Plugin horizontal pronto

--- carrousel/carrousel.js
+++ carrousel/carrousel.js
@@ -6,7 +6,9 @@ $.fn.rdjCarrousel = function(settings){
         "setas":true,
         "efeito":"horizontal",
         "automatico":true,
-        "tempoTransicao":1000
+        "tempoTransicao":1000,
+        "width":1024,
+        "height":768
     }
 
     //SUBSTITUINDO AS OPÇÕES DO DESENVOLVEDOR SOBRE O DEFAULT
@@ -19,16 +21,19 @@ $.fn.rdjCarrousel = function(settings){
         tamanho_ul = 0,
         contagem = 0,
         passando = false,
-        atual = 1;
-
-    
+        atual_direita = 0,
+        atual_esquerda = 1,
+        emTransicao = false;
 
 
     //criando a estrutura
-    $(this).wrap("<div id=\"carrousel\"><div class=\"carrousel-container\"><div class=\"carrousel\"></div></div></div>");
+    este.addClass("ul-carrousel");
+    este.wrap("<div id=\"carrousel\"><div class=\"carrousel-container\"><div class=\"carrousel\"></div></div></div>");
     
 
+    var carousel = este.parent();
     var container = este.parent().parent();
+    este.css("left",0);
 
 
 
@@ -36,28 +41,81 @@ $.fn.rdjCarrousel = function(settings){
 
     //SE EFEITO HORIZONTAL
     if(config.efeito == "horizontal"){
-        $("> li",este).each(function(){
+        $("> li",este).each(function(){       
             contagem++;
             $(this).addClass('carrousel-horizontal-li');
             $(this).attr('tamanho',$(this).width());
             $(this).attr('contagem',contagem);
             tamanho_ul += parseInt($(this).width());
+
         });
 
         este.css("width",tamanho_ul+"px");
-
+        carousel.width(config.width);
     }
+
+
+
 
     //SE OPTOU POR SETAS
 
     if(config.setas == true){
         container.append("<div class=\"carrousel-controllers\"><button data-dir=\"prev\">Previous</button><button data-dir=\"next\">Next</button></div>")
 
-        click_next = function(){
-        }
+        //ESCONDENDO SETAS
+        $(".carrousel-controllers > button[data-dir='prev']",container).hide();
 
-        click_prev = function(){
-        }
+        $(".carrousel-controllers > button[data-dir='next']",container).click(function(){
+
+            if(emTransicao == false){
+                emTransicao = true;
+                left_atual = este.css('left').replace("px","");
+                if(left_atual == "auto"){
+                    left_atual = 0;
+                }
+                if((+tamanho_ul + +left_atual) > config.width){
+                    passar = $("li[contagem="+atual_esquerda+"]",este).attr('tamanho');
+                    atual_direita++;
+                    atual_esquerda++;
+                    
+                    este.animate({
+                        left: +left_atual - passar + "px"
+                    },function(){
+                    emTransicao = false;
+                });
+                    $(".carrousel-controllers > button[data-dir='prev']",container).show();
+                    if((+tamanho_ul + (+left_atual - passar)) <= config.width) {
+                       $(".carrousel-controllers > button[data-dir='next']",container).hide(); 
+                    }
+                }
+            }
+        }); 
+
+        $(".carrousel-controllers > button[data-dir='prev']",container).click(function(){
+             if(emTransicao == false){
+                emTransicao = true;
+                left_atual = este.css('left').replace("px","");
+                if(left_atual == "auto"){
+                    left_atual = 0;
+                }
+                if((+left_atual) < 0) {
+                    passar = $("li[contagem="+atual_direita+"]",este).attr('tamanho'); //pega o atributo de tamanho da LI atual
+                    atual_direita--;
+                    atual_esquerda--;
+                    // alert(+left_atual);
+                    // alert(passar);
+                    este.animate({
+                        left: +left_atual + +passar + "px"
+                    },function(){
+                        emTransicao = false;
+                    });
+                    $(".carrousel-controllers > button[data-dir='next']",container).show();
+                    if((+left_atual + +passar) >= 0) {
+                       $(".carrousel-controllers > button[data-dir='prev']",container).hide(); 
+                    }
+                }
+            }
+        });
     }
 
 }
